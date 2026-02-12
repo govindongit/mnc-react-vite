c49bd1d2d1339ab3d414a97093f2d7df9b7db9a6
govindongit <govindnag5863@gmail.com>
2026-02-12 17:04:46 +0530

    updated the header image mapping

--- src/Components/Layout/Header/Header.jsx
+++ src/Components/Layout/Header/Header.jsx
@@ -1,11 +1,11 @@
-
-import React, { useState, useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom';
-import './header.css';
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import "./header.css";
+import { asset } from "../../../utils/asset";
 
 function Header() {
   const [clicked1, setClicked1] = useState(false); // For main menu
-  const [clicked, setClicked] = useState(false);   // For submenu
+  const [clicked, setClicked] = useState(false); // For submenu
   const menuRef = useRef(null);
   const mainMenuRef = useRef(null);
 
@@ -27,21 +27,21 @@ function Header() {
       }
     };
 
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [mainMenuRef, menuRef, setClicked1, setClicked]);
 
   return (
-    <div className='sticky-header'>
+    <div className="sticky-header">
       <style>
         {`
           body {
-            overflow-y: ${clicked1 ? 'hidden' : 'auto'};
+            overflow-y: ${clicked1 ? "hidden" : "auto"};
           }
           .header-sub-menu {
-            display: ${clicked ? 'block' : 'none'};
+            display: ${clicked ? "block" : "none"};
           }
           .sticky-header {
             position: sticky;
@@ -51,36 +51,70 @@ function Header() {
           }
         `}
       </style>
-      <div className='nav-bar'>
-        <a>
-          <img src='./src/assets/img/logo/m.png' />
-        </a>
+      <div className="nav-bar">
+        <Link to={"/"}>
+          <img src={asset("logo/m.png")} />
+        </Link>
         <div>
-          <ul id='navbar' className={clicked1 ? ' #navbar active' : '#navbar'}>
-            <li className='nav-links'><Link to={'/'} className='active'>Home</Link></li>
-            <li className='nav-links'><Link to={'/about'}>About Us</Link></li>
-            <li className='nav-links' id='our-product'>
-              <Link href='#' className='' onClick={handleClick1}>Our Product<span>+</span></Link>
-              <ul ref={menuRef} className='header-sub-menu'>
-                <li><Link to={'/acrylic'}>Acrylic And Plastics</Link></li>
-                <li><Link to={'/anatomy'}>Anatomy Models</Link></li>
-                <li><Link to={'/bags'}>Bags</Link></li>
-                <li><Link to={'/crystal'}>Crystal</Link></li>
-                <li><Link to={'/fiber'}>Fiber</Link></li>
-                <li><Link to={'/leatherite'}>Leatherite</Link></li>
-                <li><Link to={'/metal'}>Metal</Link></li>
-                <li><Link to={'/sandtimers'}>Sand Timers</Link></li>
-                <li><Link to={'/shirtandtShirt'}>Shirts & T-Shirts</Link></li>
-                <li><Link to={'/umbrella'}>Umbrella</Link></li>
-                <li><Link to={'/spacepens'}>Space Pens</Link></li>
+          <ul id="navbar" className={clicked1 ? " #navbar active" : "#navbar"}>
+            <li className="nav-links">
+              <Link to={"/"} className="active">
+                Home
+              </Link>
+            </li>
+            <li className="nav-links">
+              <Link to={"/about"}>About Us</Link>
+            </li>
+            <li className="nav-links" id="our-product">
+              <Link href="#" className="" onClick={handleClick1}>
+                Our Product<span>+</span>
+              </Link>
+              <ul ref={menuRef} className="header-sub-menu">
+                <li>
+                  <Link to={"/acrylic"}>Acrylic And Plastics</Link>
+                </li>
+                <li>
+                  <Link to={"/anatomy"}>Anatomy Models</Link>
+                </li>
+                <li>
+                  <Link to={"/bags"}>Bags</Link>
+                </li>
+                <li>
+                  <Link to={"/crystal"}>Crystal</Link>
+                </li>
+                <li>
+                  <Link to={"/fiber"}>Fiber</Link>
+                </li>
+                <li>
+                  <Link to={"/leatherite"}>Leatherite</Link>
+                </li>
+                <li>
+                  <Link to={"/metal"}>Metal</Link>
+                </li>
+                <li>
+                  <Link to={"/sandtimers"}>Sand Timers</Link>
+                </li>
+                <li>
+                  <Link to={"/shirtandtShirt"}>Shirts & T-Shirts</Link>
+                </li>
+                <li>
+                  <Link to={"/umbrella"}>Umbrella</Link>
+                </li>
+                <li>
+                  <Link to={"/spacepens"}>Space Pens</Link>
+                </li>
               </ul>
             </li>
-            <li className='nav-links'><Link to={'/newarrival'}>New Arrivals</Link></li>
-            <li className='nav-links'><Link to={'/contact'}>Contact</Link></li>
+            <li className="nav-links">
+              <Link to={"/newarrival"}>New Arrivals</Link>
+            </li>
+            <li className="nav-links">
+              <Link to={"/contact"}>Contact</Link>
+            </li>
           </ul>
         </div>
-        <div id='mobile' onClick={handleClick}>
-          <i className={clicked1 ? 'fas fa-times' : 'fas fa-bars'}></i>
+        <div id="mobile" onClick={handleClick}>
+          <i className={clicked1 ? "fas fa-times" : "fas fa-bars"}></i>
         </div>
       </div>
     </div>
